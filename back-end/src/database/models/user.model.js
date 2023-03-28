c3ba4bbb365dff8132b3f366c55a18c0bf640b71
thllz <thelisommagnos@hotmail.com>
2023-03-28 16:04:45 -0300

    feat: userModel && migration

---
+++ back-end/src/database/models/user.model.js
@@ -0,0 +1,23 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false },
+  }, {
+    timestamp: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
