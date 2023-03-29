8cbe8af57725cd4731a43bce1c284b1236448cb1
Lucas Vieira <ls6182315@gmail.com>
2023-03-29 19:48:58 -0300

    register path

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/register" element={ <Register /> } />
     </Routes>
   );
 }
