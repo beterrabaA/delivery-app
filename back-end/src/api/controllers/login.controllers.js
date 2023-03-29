b7d9063413a5688276a46d8c0926794f2b40119c
Lucas Vieira <ls6182315@gmail.com>
2023-03-29 19:47:50 -0300

    add register controller

--- back-end/src/api/controllers/login.controllers.js
+++ back-end/src/api/controllers/login.controllers.js
@@ -1,3 +1,5 @@
+const { encode } = require('../../utils/token');
+
 const loginService = require('../services/login.services');
 
 const login = async (req, res) => {
@@ -7,6 +9,16 @@ const login = async (req, res) => {
   return res.status(200).json(service);
 };
 
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const encodedPassword = encode(password);
+  console.log(encodedPassword);
+  const service = await loginService.register({ name, email, password: encodedPassword });
+
+  return res.status(201).json(service);
+};
+
 module.exports = {
   login,
+  register,
 };
